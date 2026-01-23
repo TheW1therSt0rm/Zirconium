e43033e9e96748b753dd9b47af7bc0dc3fbd29af
TheW1therSt0rm <marco.wither@hotmail.com>
2026-01-23 21:05:45 +0000

    Init2

--- obj/Debug/net8.0/RayTracing.AssemblyInfo.cs
+++ obj/Debug/net8.0/RayTracing.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RayTracing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+536f2e0a3a0a7f94e1b2f84c59c0c092e37ce735")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c141056cfe503c86d5567ec90b45cffa9a8b2bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("RayTracing")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RayTracing")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
